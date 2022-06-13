f3f88e26fe8b25672c18f1493795dd1ad5ae920a
Brien Thomas <thomasbrien94@gmail.com>
2022-06-13 15:20:01 -0700

    framework of App.js created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import FoodDropdown from './FoodDropdown.js';
+import SideDropdown from './SideDropdown.js';
+import DrinkDropdown from './DrinkDropdown.js';
+import DisplayOrder from './DisplayOrder.js';
 
 function App() {
+
+  const [food, setFood] = useState('sushi');
+  const [side, setSide] = useState('Sweet Potato Fries');
+  const [drink, setDrink] = useState('Thai Iced Tea');
+
+  const [instructions, setInstructions] = useState([]);
+  const [InstructionForm, setInstructionForm] = useState('');
+
+  const [orderName, setOrderName] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Create Order Section</h3>
+      <section>
+        Dropdown Section
+        <FoodDropdown />
+        <SideDropdown />
+        <DrinkDropdown />
+      </section>
+
+      <hr />
+      <section>
+
+        <h3>Display Order Section</h3>
+        <DisplayOrder 
+          orderName={orderName}
+          food={food} 
+          side={side}
+          drink={drink}
+          instructions={instructions} />
+      </section>
     </div>
   );
 }
