2ba92429500aba61835e65767b184c85b4070d51
Brien Thomas <thomasbrien94@gmail.com>
2022-06-14 17:29:19 -0700

    Deleted unnecessary blank line

--- src/InstructionForm.js
+++ src/InstructionForm.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default function InstructionForm({ handleSubmit, setInstructionForm, instructionForm }) {
   return (
     <form onSubmit={handleSubmit}>
-
       <label>Add Instructions: 
         <textarea value={instructionForm} onChange={(e) => setInstructionForm(e.target.value)} />
       </label>
